965a865c5a44d811d023755715ef83545d657ffa
matyhp2 <matyhp2@gmail.com>
2025-10-03 01:14:44 -0300

    index armado

---
+++ backend/src/index.ts
@@ -0,0 +1,61 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import { Request, Response } from 'express';
+import feedlotRutas from './presentation/routes/feedlot-rutas';
+import localidadRutas from './presentation/routes/localidad-rutas';
+import provinciaRutas from './presentation/routes/provincia-rutas';
+import usuarioRutas from './presentation/routes/usuario-rutas';
+import personalRutas from './presentation/routes/personal-rutas';
+import enfermedadRutas from './presentation/routes/enfermedad-rutas';
+import trataminetoRutas from './presentation/routes/tratamiento-rutas'
+import corralRoutes from './presentation/routes/corral.routes'; 
+import detalleAlimentoRoutes from "./presentation/routes/detalleAlimento.routes";
+import suministroRoutes from "./presentation/routes/suministro.routes"
+
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    origin: 'http://localhost:3000', // Adjust as needed
+    credentials: true,
+}));
+
+//Rutas 
+
+app.use("/api/auth", usuarioRutas);
+app.use("/api/localidades", localidadRutas);
+app.use("/api/feedlots", feedlotRutas);
+app.use("/api/provincias", provinciaRutas);
+app.use("/api/personal", personalRutas);
+app.use("/api/enfermedades", enfermedadRutas);
+app.use("/api/tratamientos", trataminetoRutas);
+app.use('/corrales', corralRoutes);
+app.use('/detalle-alimentos', detalleAlimentoRoutes);
+app.use('/suministros', suministroRoutes);
+
+
+// app.use("/api/provincias", provinciaRutas);
+
+
+
+app.get("/test-provincia", (req, res) => {
+  res.send("Ruta directa funcionando");
+});
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello, World!');
+});
+
+
+//inicializar el servidor
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
